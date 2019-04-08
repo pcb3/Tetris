a7e03a528ff2e80e91799eab99c7d60c27bdf18f
pcb3 <petercraigblaas@gmail.com>
2019-04-09 11:19:50 +1200

    Design tetris-render and add .rkt to filename

---
+++ tetris.rkt
@@ -0,0 +1,130 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname tetris) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; A simple tetris clone
+
+; requires
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; physical constants
+(define WIDTH 10) ; # of blocks, horizontally
+(define SIZE 10) ; blocks are sqaures
+(define SCENE-SIZE (* WIDTH SIZE))
+
+; graphical constants
+(define BLOCK ; red squares with black rims
+  (overlay
+   (square (- SIZE 1) 'solid 'red)
+   (square SIZE 'outline 'black)))
+(define MT (empty-scene SCENE-SIZE SCENE-SIZE))
+
+(define-struct tetris [block landscape])
+(define-struct block [x y])
+
+; A Tetris is a structure:
+; (make-tetris Block Landscape)
+; A Landscape is one of:
+; - '()
+; - (cons Block Landscape)
+; A Block is a structure:
+; (make-block N N)
+
+; interpretations
+; (make-block x y) depicts a block whose left
+; corner is (* x SIZE) pixels from the left
+; (* y SIZE) pixels from the top;
+; (make-tetris b0 (list b1 b2 ...)) means b0 is the
+; dropping block, while b1, b2, and ... are resting
+
+; instance of data collection
+
+; Tetris:
+(define tetris0 (make-tetris (make-block 1 1) '()))
+
+(define tetris1 (make-tetris (make-block 5 5)
+                             (cons (make-block 2 2) '())))
+
+(define tetris3 (make-tetris (make-block 3 3)
+                             (cons (make-block 4 4)
+                                   (cons (make-block 8 8)
+                                         (cons (make-block 10 10)
+                                               '())))))
+
+; Landscape
+(define landscape0 '())
+
+(define landscape1 (cons (make-block 1 1) '()))
+
+; Block
+(define block-dropping (make-block 0 3))
+(define block0 (make-block 0 0))
+(define block1 (make-block 1 1))
+(define block2 (make-block 10 10))
+(define block3 (make-block 5 5))
+
+; Tetris -> Image
+(check-expect (tetris-render
+               (make-tetris (make-block 5 5) '()))
+              (place-image BLOCK 50 50 MT))
+
+(check-expect (tetris-render
+               (make-tetris (make-block 0.5 0.5)
+                            (cons (make-block 0.5 9.5)
+                                  (cons (make-block 1.5 9.5)
+                                        (cons (make-block 2.5 9.5)
+                                              '())))))
+              (place-images
+               (list BLOCK BLOCK BLOCK BLOCK)
+               (list (make-posn 5 5) (make-posn 5 95) (make-posn 15 95)
+                     (make-posn 25 95))
+               MT))
+               
+(define (fn-tetris-render t)
+  (cond
+    [(empty? (tetris-landscape t))
+    (... BLOCK
+     (... ... (block-x (tetris-block t)))
+     (... ... (block-y (tetris-block t)))
+     MT)]
+    [else (... BLOCK
+               (... (block-x (first (tetris-landscape t))))
+               (... (block-y (first (tetris-landscape t))))
+               (fn-tetris-render
+                (make-tetris (tetris-block t)
+                             (rest (tetris-landscape t)))))]))
+                             
+(define (tetris-render t)
+  (cond
+    [(empty? (tetris-landscape t))
+    (place-image BLOCK
+     (* 10 (block-x (tetris-block t)))
+     (* 10 (block-y (tetris-block t)))
+     MT)]
+    [else (place-image BLOCK
+               (* 10 (block-x (first (tetris-landscape t))))
+               (* 10 (block-y (first (tetris-landscape t))))
+               (tetris-render
+                (make-tetris (tetris-block t)
+                             (rest (tetris-landscape t)))))]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
